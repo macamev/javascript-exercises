6cc7b90e7eade001e80dc1ca49fe1b8c7ecfa724
macamev <shmaxim@tuta.io>
2024-12-22 20:30:10 +0200

    First 2 javascript exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+let word = 'hey'
+const repeatString = function(word, amount) {
+    if (amount < 0){
+        return 'ERROR'
+    }
+    let newWord = ''
+    for (let i = 1; i <= amount; i++) { // Corrected for loop syntax
+        newWord += word
+    }
+    return newWord;
 };
 
 // Do not edit below this line
